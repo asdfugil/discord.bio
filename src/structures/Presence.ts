f47267af2fe5d79553339b8299827f7ac48be746
Assfugil <42699250+Assfugil@users.noreply.github.com>
2020-07-14 10:27:46 +0800

    Fix presence

--- src/structures/Presence.ts
+++ src/structures/Presence.ts
@@ -3,7 +3,10 @@ import { ClientPresenceStatus, PresenceStatus } from 'discord.js'
 import { Bio, Activity, User } from '..'
 /**Represents a user's presence. */
 class Presence extends Base {
-  /**The devices this presence is on */
+  /**
+   * The devices this presence is on
+   * @deprecated The webscoket no longer provide this information
+  */
   clientStatus: {
     /**The current presence in the web application */
     web?: ClientPresenceStatus,
@@ -20,14 +23,10 @@ class Presence extends Base {
   status: PresenceStatus
   constructor(bio: Bio, data: any) {
     super(bio)
-    this.clientStatus = data.status || {}
+    this.clientStatus =  {}
     this.activity = data.activity || null
     this.user = data.user
-    this.status = 'offline'
-    const statuses = Object.values(data.status || {})
-    if (statuses.includes('online')) this.status = 'online'
-    else if (statuses.includes('dnd')) this.status = 'dnd'
-    else if (statuses.includes('idle')) this.status = 'idle'
+    this.status = data.status
   }
   equals(presence: Presence): boolean {
     return this === presence ||
